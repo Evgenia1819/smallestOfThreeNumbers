2176266b0a3692eb95fcccbf121ea72905cf1d9b
Evgenia1819 <95176074+Evgenia1819@users.noreply.github.com>
2021-12-19 09:35:24 +0000

    Add files via upload

---
+++ smallestOfThreeNumbers.js
@@ -0,0 +1,10 @@
+function getSmallestNumber (firstNumber, secondNumber, thirdNumber) {
+let smallestNumber = Math.min(firstNumber, secondNumber, thirdNumber);
+return smallestNumber;
+
+}
+
+let result = getSmallestNumber(2, 5, 3);
+console.log(result);
+getSmallestNumber(600, 342, 123);
+getSmallestNumber(25, 21, 4);
